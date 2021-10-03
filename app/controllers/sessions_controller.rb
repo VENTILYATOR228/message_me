345d1ad2a8aa76683fd4fe70a16faaf6fe0f7747
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-10-03 12:36:08 +0300

    Add sessions create and destroy action & navigation to navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in. Welcome to chat, #{user.username}"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out. Good bye!"
+    redirect_to login_path
   end
 
 end
