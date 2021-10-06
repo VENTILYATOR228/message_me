0fe9cad1b4a9fc983ccac4a0c2fd351e8d61205a
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-10-06 09:53:44 +0300

    Add sign up and edit account functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,57 @@
 class UsersController < ApplicationController
-
-  def show
-
-  end
-
-  def index
-
-  end
+  before_action :set_user, only: [:edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
-
+    @user = User.new
   end
 
   def edit
-
   end
 
   def update
-
+    if @user.update(user_params)
+      flash[:success] = "Your username was successfully updated"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @user.destroy
+    session[:user_id] = nil if @user == current_user
+    flash[:success] = "Account was deleted"
+    redirect_to login_path
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:error] = "You can only update your own account"
+      redirect_to root_path
+    end
   end
 
 end
